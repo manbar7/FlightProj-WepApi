579c3e4f964673e156b1acefb749337bb0c7b85a
manbar7 <45047021+manbar7@users.noreply.github.com>
2020-01-17 18:18:11 +0200

    Add files via upload

---
+++ ClassLibrary1/DAOMSSQL/ResetDAO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.DAOMSSQL
+{
+    class ResetDAO : IResetDAO
+    {
+        ResetDAO resetDAO = new ResetDAO();
+
+        public static SqlCommand cmd = new SqlCommand();
+
+        public IList<Flight> DeleteOverdueFlights()
+        {
+            IList<Flight> resultFlights = new List<Flight>();
+            using (cmd.Connection = new SqlConnection(FlightCenterConfig.ConnectionString))
+            {
+                cmd.Connection.Open();
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = $"SELECT * FROM FLIGHTS";
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Flight CurrentresultFlight = new Flight()
+                        {
+                            FLIGHT_ID = (int)reader["ID"],
+                            AIRLINECOMPANY_ID = (int)reader["AIRLINECOMPANY_ID"],
+                            ORIGIN_COUNTRY_CODE = (int)reader["ORIGIN_COUNTRY_CODE"],
+                            DESTINATION_COUNTRY_CODE = (int)reader["DESTINATION_COUNTRY_CODE"],
+                            DEPARTURE_TIME = (DateTime)reader["DEPARTURE_TIME"],
+                            LANDING_TIME = (DateTime)reader["LANDING_TIME"],
+                            REMAINING_TICKETS = (int)reader["REMAINING_TICKETS"]
+
+                            
+                        };
+                        DateTime t1 = CurrentresultFlight.LANDING_TIME.;
+                        DateTime t2 = DateTime.Now;
+                        int sum = t1 - t2;
+                        if ((t2 - t1) = 3)
+
+                            resultFlights.Remove(CurrentresultFlight);
+                    }
+                }
+            }
+            return resultFlights;
+        }
+    }
+}
---
+++ ClassLibrary1/FlyingCenterSystem.cs
@@ -0,0 +1,103 @@
+﻿using ClassLibrary1.DAOMSSQL;
+using ClassLibrary1.Facade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Threading.Timer;
+
+namespace ClassLibrary1
+{ 
+
+    public sealed class FlyingCenterSystem
+    {
+
+        private static FlyingCenterSystem _instance;
+        private static object key = new object();
+        private LoginService login;
+        private Timer timer;
+        List<Flight> FlightHistory = new List<Flight>();
+        List<Ticket> TicketHistory = new List<Ticket>();
+
+
+        
+        
+
+        private FlyingCenterSystem()
+        {
+            login = new LoginService();
+
+                timer = new Timer(new TimerCallback(TimerElapsed), null, 5000, FlightCenterConfig.ResetDAO);
+        }
+
+        private void TimerElapsed(object state)
+        {
+            
+        }
+
+        public static FlyingCenterSystem GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (key)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new FlyingCenterSystem();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+
+
+        public ILoginToken Login(string username, string password)
+        {
+
+            if (login.TryAdminLogin(username, password, out LoginToken<Administrator> adminToken))
+            {
+                return adminToken;
+            }
+            if (login.TryCustomerLogin(username, password, out LoginToken<Customer> customerToken))
+            {
+                return customerToken;
+            }
+            if (login.TryAirlineLogin(username, password, out LoginToken<AirlineCompany> airlineToken))
+            {
+                return airlineToken;
+            }
+            else
+            {
+                throw new UserNotFoundException($"user name:{username} and password:{password} doesn't match");
+            }
+        }
+
+        public FacadeBase GetFacade(ILoginToken token)
+        {
+            if (token == null)
+            {
+                return new AnonymousUserFacade();
+            }
+            if (token.GetType() == typeof(LoginToken<Administrator>))
+            {
+                return new LoggedInAdministratorFacade();
+            }
+            if (token.GetType() == typeof(LoginToken<Customer>))
+            {
+                return new LoggedInCustomerFacade();
+            }
+            if (token.GetType() == typeof(LoginToken<AirlineCompany>))
+            {
+                return new LoggedInAirlineFacade();
+            }
+            return new AnonymousUserFacade();
+        }
+
+
+
+    }
+}
